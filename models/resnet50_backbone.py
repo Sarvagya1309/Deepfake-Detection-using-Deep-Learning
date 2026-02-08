d5e60290e8fa94de21b89496db9490b7b8f637b6
Sarvagya1309 <152787578+Sarvagya1309@users.noreply.github.com>
2026-02-08 18:20:18 +0530

    Update resnet50_backbone.py

--- models/resnet50_backbone.py
+++ models/resnet50_backbone.py
@@ -1,1 +1,10 @@
+# models/resnet50_backbone.py
+import torch
+from torchvision.models import resnet50
+
+def get_resnet50(device):
+    model = resnet50(pretrained=True)
+    model.fc = torch.nn.Identity()
+    model.eval()
+    return model.to(device)
 
